50f5b83877d84beb410ca8a5fe4756226fa2084e
gg <lkg61230413@163.com>
2018-12-25 13:15:51 +0800

    修正拼写错误

--- lore-conf/src/main/java/im/heart/conf/SpringSessionConfig.java
+++ lore-conf/src/main/java/im/heart/conf/SpringSessionConfig.java
@@ -3,5 +3,5 @@ package im.heart.conf;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
-public class SpringSessionCOnfig {
+public class SpringSessionConfig {
 }
