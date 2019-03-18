574ca388ed0ef75bb3dfbc1503a66def5fba806c
gg <lkg61230413@163.com>
2019-03-18 18:01:12 +0800

    增加注释

--- lore-conf/src/main/java/im/heart/conf/SpringSessionConfig.java
+++ lore-conf/src/main/java/im/heart/conf/SpringSessionConfig.java
@@ -2,6 +2,11 @@ package im.heart.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+/**
+ *
+ * @author gg
+ * @desc spring session 配置
+ */
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds=60*60*24*7)
 public class SpringSessionConfig {
